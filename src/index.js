5398bdae188b6f8dbc46441ab374d7c385187a8a
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-03-10 02:24:19 +0500

    feat: base variant on stack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  const checkOpenBracket = (char) => {
+    bracketsConfig.forEach(item => {
+      let isCharOpenBracket = item[0] === char;
+
+      if (isCharOpenBracket) {
+        let isBracketsEquals = item[0] === item[1],
+            isStackHeadEqualsOpenBracket = stack[stack.length - 1] === item[0]
+
+        if (!isBracketsEquals || (isBracketsEquals && !isStackHeadEqualsOpenBracket)) {
+          stack.push(char)
+        }
+      }
+    })
+  }
+
+  const checkCloseBracket = (char) => {
+    for (let i=0, length = bracketsConfig.length; i < length; i++) {
+      let item = bracketsConfig[i]
+
+      if (char === item[1]) {
+        let lastOpenBracket = stack.pop();
+        console.log(lastOpenBracket, char)
+
+        if (lastOpenBracket !== item[0]) {
+          console.log('finita', lastOpenBracket, item[0], char)
+          return false;
+        } 
+      }
+    }
+
+    return true;
+  }
+
+  for (let i=0, length = str.length; i < length; i++) {
+    let char = str[i];
+
+    checkOpenBracket(char);
+    if (!checkCloseBracket(char)) {
+      return false;
+    }
+  }
+
+  if (stack.length > 0) {
+    return false;
+  }
+  
+  return true;
 }
