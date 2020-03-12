8c9c2b4c1c714cf57e0fe3c083665b2a425a8ab4
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-03-13 04:15:37 +0500

    fix: create fix for twins brackets

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ module.exports = function check(str, bracketsConfig) {
   let stack = [];
 
   const checkOpenBracket = (char) => {
-    bracketsConfig.forEach(item => {
-      let isCharOpenBracket = item[0] === char;
+    for (let i = 0, length = bracketsConfig.length; i < length; i++) {
+      let item = bracketsConfig[i];
+      let isCharOpenBracket = char === item[0];
 
       if (isCharOpenBracket) {
         let isBracketsEquals = item[0] === item[1],
-            isStackHeadEqualsOpenBracket = stack[stack.length - 1] === item[0]
+            isStackHaveEqualsOpenBracket = stack.findIndex(item => item === char) > -1;
 
-        if (!isBracketsEquals || (isBracketsEquals && !isStackHeadEqualsOpenBracket)) {
+        if (!isBracketsEquals || (isBracketsEquals && !isStackHaveEqualsOpenBracket)) {
           stack.push(char)
+          return true;
         }
       }
-    })
+    }
+
+    return false;
   }
 
   const checkCloseBracket = (char) => {
@@ -22,10 +26,8 @@ module.exports = function check(str, bracketsConfig) {
 
       if (char === item[1]) {
         let lastOpenBracket = stack.pop();
-        console.log(lastOpenBracket, char)
 
         if (lastOpenBracket !== item[0]) {
-          console.log('finita', lastOpenBracket, item[0], char)
           return false;
         } 
       }
@@ -37,9 +39,10 @@ module.exports = function check(str, bracketsConfig) {
   for (let i=0, length = str.length; i < length; i++) {
     let char = str[i];
 
-    checkOpenBracket(char);
-    if (!checkCloseBracket(char)) {
-      return false;
+    if (!checkOpenBracket(char)) {
+      if (!checkCloseBracket(char)) {
+        return false;
+      }
     }
   }
 
